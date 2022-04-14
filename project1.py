439a8b8168aa09a89ed8d1c826b257c1bea144cd
Atharva Patil <atharvapatil0052@gmail.com>
2022-04-15 00:19:16 +0530

    🧱 Added colorpicker

--- project1.py
+++ project1.py
@@ -3,10 +3,21 @@ import cv2
 frameWidth = 640
 frameHeight = 480
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
-cap.set(10,130)
+cap.set(10,150) # to set brightness
+
+myColors = [] # list of colours that we want to detect using cam
+# we need to give it the minimum and maximum hue values
+
+
+def findColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    cv2.imshow("img",mask)
 
 while True:
     success, img = cap.read()
