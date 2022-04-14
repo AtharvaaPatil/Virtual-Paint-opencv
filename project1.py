0a9ec4b656a73f059edbde6956cdac03e3659100
Atharva Patil <atharvapatil0052@gmail.com>
2022-04-15 02:10:10 +0530

    ✨ Project Completed

--- project1.py
+++ project1.py
@@ -14,18 +14,29 @@ myColors = [[5,107,0,19,255,255],
             [57,76,0,100,255,255]] # list of colours that we want to detect using cam
 # we need to give it the minimum and maximum hue values
 
-myColorValues = [[]] # It is BGR not RGB here
+myColorValues = [[51,135,255],
+                [255,0,255],
+                [0,255,0]] # It is BGR not RGB here
 
 
-def findColor(img):
+myPoints = [] #[x, y, colorId] see x and y and draw a circle of that colorid at that point
+
+
+def findColor(img,myColors,myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    count = 0
+    newPoints=[]
     for color in myColors:
         lower = np.array(color[0][0:3])
         upper = np.array(color[0][3:6])
         mask = cv2.inRange(imgHSV,lower,upper)
         x,y = getContours(mask)
-        cv2.circle(img,Result,(x,y),10,(255,0,0),cv2.FILLED)
-        cv2.imshow(str(color[0]),mask)
+        cv2.circle(img,Result,(x,y),10,myColorValues[count],cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x,y,count])
+        count += 1
+        # cv2.imshow(str(color[0]),mask)
+    return newPoints # this is a list
 
 def getContours(img):
     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
@@ -39,10 +50,20 @@ def getContours(img):
             x, y, w, h = cv2.boundingRect(approx)
     return x+w//2,y # gives us the top point instead of the centre of the bounding box so we can draw properly
 
+def drawOnCanvas(myPoints,myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult,point[0],point[1],10,myColorValues[point[2]],cv2.FILLED)
+
 while True:
     success, img = cap.read()
     imgResult = img.copy()
-    findColor(img, myColors)
+    newPoints = findColor(img, myColors, myColorValues)
+    if len(newPoints) != 0:
+        for newP in newPoints: # we used append so we can convert the list inside a list to just points
+            myPoints.append(newP)
+    if len(myPoints)!=0:
+        drawOnCanvas(myPoints,myColorValues)
+
     cv2.imshow("Result",imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
